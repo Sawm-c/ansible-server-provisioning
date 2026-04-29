1ea96e46d54bba8d72b51cf3a1724d46d1e0474a
Sawm-c <tanh3489@gmail.com>
2026-04-30 01:52:15 +0700

    fix: install passlib on EC2 to support password hashing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,15 +13,10 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro" # Free tier eligible
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the AWS EC2 Key Pair to allow SSH access"
-  type        = string
-}
-
-variable "public_key" {
-  description = "Public key content for SSH access"
+  description = "Name of the AWS EC2 Key Pair to allow SSH access (must already exist in AWS)"
   type        = string
 }
